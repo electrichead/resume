3f0e7b9bc5c739deb2a5ddc539810b01131ea2ca
Nitin Vericherla <nitin.vericherla@gmail.com>
2024-03-28 11:14:59 -0400

    Updates March 2024

--- package-scripts.js
+++ package-scripts.js
@@ -1,16 +1,7 @@
-const npsUtils = require('nps-utils');
-const { resolve: pathResolve } = require('path');
-
-const yargs = require('yargs/yargs');
-const { hideBin } = require('yargs/helpers');
-const argv = yargs(hideBin(process.argv)).argv;
-
-console.log({ argv });
-
 module.exports = {
   scripts: {
     default: 'build',
     build:
-      'docker run --rm -v $PWD:/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf ./resume.adoc --theme=src/theme-new.yml -a pdf-fontsdir=assets/fonts -o ./dist/nv-resume.pdf',
+      'docker run --rm -v $PWD:/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf ./src/resume-202403.adoc --theme=src/theme-new.yml -a pdf-fontsdir=assets/fonts -o ./dist/nv-resume.pdf',
   },
 };
